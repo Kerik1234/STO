e640baa3f54840457b5c4361adc3fec6eef09bd1
Kerik1234 <kirillmans2004@gmail.com>
2025-06-24 09:09:46 +0300

    3 обновление

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,13 +1,30 @@
 const swiper = new Swiper('.swiper', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // мобильные
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      // планшеты
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
-    });
+      // десктоп
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
+  });
